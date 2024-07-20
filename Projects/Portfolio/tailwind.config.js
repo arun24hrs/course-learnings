3a71b6502790633220fb56b65d52c902aee6a369
arun24hrs <2017arun.rana@gmail.com>
2024-07-20 13:33:41 +0530

    creating portfolio

---
+++ Projects/Portfolio/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        theme: {
+          10: '#eae2b7',
+        20: '#fcbf49',
+        30: '#f77f00',
+        40: '#d62828',
+        45: '#003049'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
