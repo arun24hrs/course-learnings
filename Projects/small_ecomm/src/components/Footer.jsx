ca050119810039949ad6fe624e8488a0f441ada9
arun24hrs <2017arun.rana@gmail.com>
2025-01-20 23:18:06 +0530

    added github link

--- Projects/small_ecomm/src/components/Footer.jsx
+++ Projects/small_ecomm/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div id="footer">
         <p>Made with ❤️ by Arun</p>
-        <p>Github Link: </p>
+        <p>Github Link: <a href="https://github.com/arun24hrs/course-learnings/tree/master/Projects/small_ecomm">Github Link</a></p>
     </div>
   )
 }
